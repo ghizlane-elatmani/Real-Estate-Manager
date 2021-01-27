df968d9a8e6f18cdd3081131ff1933e3638a5fcc
Ghizlane El atmani <ghizlane.elatmani.99@gmail.com>
2021-01-27 21:23:23 +0100

    0.2.0 - Improved functionality provided: Convert Euro to Dollar

--- app/src/main/java/com/openclassrooms/realestatemanager/Utils.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/Utils.java
@@ -23,6 +23,10 @@ public class Utils {
         return (int) Math.round(dollars * 0.812);
     }
 
+    public static int convertEuroToDollar(int euros){
+        return (int) Math.round(euros / 0.812);
+    }
+
     /**
      * Conversion de la date d'aujourd'hui en un format plus approprié
      * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
