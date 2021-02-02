62cecb9507c00924a880c060b3aef1a9b2bd4b07
Ghizlane El atmani <ghizlane.elatmani.99@gmail.com>
2021-02-02 16:23:27 +0100

    1.10.0 - Add method in EstateDataRepository

--- app/src/main/java/com/openclassrooms/realestatemanager/repository/EstateDataRepository.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/repository/EstateDataRepository.java
@@ -1,4 +1,35 @@
 package com.openclassrooms.realestatemanager.repository;
 
+import androidx.lifecycle.LiveData;
+
+import com.openclassrooms.realestatemanager.database.dao.EstateDao;
+import com.openclassrooms.realestatemanager.model.Estate;
+
+import java.util.List;
+
 public class EstateDataRepository {
+
+    private final EstateDao estateDao;
+
+    public EstateDataRepository(EstateDao estateDao){
+        this.estateDao = estateDao;
+    }
+
+    LiveData<Estate> getEstate(int id){
+        return estateDao.getEstate(id);
+    }
+
+    LiveData<List<Estate>> getAllEstates(){
+        return estateDao.getAllEstates();
+    }
+
+    void insertEstate(Estate estate){
+        estateDao.insertEstate(estate);
+    }
+
+    void updateEstate(Estate estate){
+        estateDao.updateEstate(estate);
+    }
+
+
 }
