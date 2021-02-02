701e9020a2925f04e15c7a1d58ae8fcbc3973c2a
Ghizlane El atmani <ghizlane.elatmani.99@gmail.com>
2021-02-02 17:08:14 +0100

    1.13.0 - Add method to ViewModelFactory.java

--- app/src/main/java/com/openclassrooms/realestatemanager/injection/ViewModelFactory.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/injection/ViewModelFactory.java
@@ -1,4 +1,32 @@
 package com.openclassrooms.realestatemanager.injection;
 
-public class ViewModelFactory {
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.openclassrooms.realestatemanager.repository.AgentDataRepository;
+import com.openclassrooms.realestatemanager.repository.EstateDataRepository;
+import com.openclassrooms.realestatemanager.repository.PhotoDataRepository;
+
+import java.util.concurrent.Executor;
+
+public class ViewModelFactory implements ViewModelProvider.Factory {
+
+    private final AgentDataRepository agentDataSource;
+    private final EstateDataRepository estateDataSource;
+    private final PhotoDataRepository photoDataSource;
+    private final Executor executor;
+
+    public ViewModelFactory(AgentDataRepository agentDataSource, EstateDataRepository estateDataSource, PhotoDataRepository photoDataSource, Executor executor){
+        this.agentDataSource = agentDataSource;
+        this.estateDataSource = estateDataSource;
+        this.photoDataSource = photoDataSource;
+        this.executor = executor;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        return null;
+    }
 }
