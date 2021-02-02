3616798862c7bd96f377d340905717d5e2725f6f
Ghizlane El atmani <ghizlane.elatmani.99@gmail.com>
2021-02-02 16:26:28 +0100

    1.11.0 - Add method in PhotoDataRepository

--- app/src/main/java/com/openclassrooms/realestatemanager/repository/PhotoDataRepository.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/repository/PhotoDataRepository.java
@@ -1,4 +1,40 @@
 package com.openclassrooms.realestatemanager.repository;
 
+import androidx.lifecycle.LiveData;
+
+import com.openclassrooms.realestatemanager.database.dao.PhotoDao;
+import com.openclassrooms.realestatemanager.model.Photo;
+
+import java.util.List;
+
 public class PhotoDataRepository {
+
+    private final PhotoDao photoDao;
+
+    public PhotoDataRepository(PhotoDao photoDao){
+        this.photoDao = photoDao;
+    }
+
+
+    public LiveData<List<Photo>> getAllPhotos(){
+        return photoDao.getAllPhotos();
+    }
+
+    public LiveData<List<Photo>> getPhotos(int estate_id){
+        return photoDao.getPhotos(estate_id);
+    }
+
+    public void insertPhoto(Photo photo){
+        photoDao.insertPhoto(photo);
+    }
+
+    public void updateEstatePhoto(Photo photo){
+        photoDao.updateEstatePhoto(photo);
+    }
+
+    public void deleteEstatePhoto(Photo photo){
+        photoDao.deleteEstatePhoto(photo);
+    }
+
+
 }
