d88cb4caac321dc6c4d67d2a7843c014c40511ce
Ghizlane El atmani <ghizlane.elatmani.99@gmail.com>
2021-02-02 11:02:14 +0100

    1.6.0 - Adding some methods in EstateDao

--- app/src/main/java/com/openclassrooms/realestatemanager/database/dao/EstateDao.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/database/dao/EstateDao.java
@@ -1,4 +1,26 @@
 package com.openclassrooms.realestatemanager.database.dao;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.openclassrooms.realestatemanager.model.Estate;
+
+import java.util.List;
+
 public interface EstateDao {
+
+    @Query("SELECT * FROM estate WHERE id = :id")
+    LiveData<Estate> getEstate(int id);
+
+    @Query("SELECT * FROM estate")
+    LiveData<List<Estate>> getAllEstates();
+
+    @Insert
+    long insertEstate(Estate estate);
+
+    @Update
+    void updateEstate(Estate estate);
+
 }
