36553130393f68af726a5a7fb0b14c7fedec4f2e
Ghizlane El atmani <ghizlane.elatmani.99@gmail.com>
2021-02-02 16:21:51 +0100

    1.9.0 - Add method in AgentDataRepository

--- app/src/main/java/com/openclassrooms/realestatemanager/repository/AgentDataRepository.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/repository/AgentDataRepository.java
@@ -1,4 +1,22 @@
 package com.openclassrooms.realestatemanager.repository;
 
+import androidx.lifecycle.LiveData;
+
+import com.openclassrooms.realestatemanager.database.dao.AgentDao;
+import com.openclassrooms.realestatemanager.model.Agent;
+
+import java.util.List;
+
 public class AgentDataRepository {
+
+    private final AgentDao agentDao;
+
+    public AgentDataRepository(AgentDao agentDao){
+        this.agentDao = agentDao;
+    }
+
+    public LiveData<List<Agent>> getAllAgent(){
+        return agentDao.getAllAgents();
+    }
+
 }
