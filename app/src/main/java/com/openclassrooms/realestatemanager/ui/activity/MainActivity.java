2ea15ef60d2ed284cf233406f98aaec6ea69ff2a
Ghizlane El atmani <ghizlane.elatmani.99@gmail.com>
2021-02-05 17:58:05 +0100

    1.15.0 - Layout menu

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/activity/MainActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/activity/MainActivity.java
@@ -2,17 +2,26 @@ package com.openclassrooms.realestatemanager.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.openclassrooms.realestatemanager.R;
+import com.openclassrooms.realestatemanager.databinding.ActivityMainBinding;
 import com.openclassrooms.realestatemanager.utils.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
+    // --- Attribute ---
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
 
     }
 
